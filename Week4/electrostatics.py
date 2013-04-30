aedd3699c352f8f389aa7a026a8a0ef8f2816843
George Jing <gjing@calpoly.edu>
2013-04-29 20:15:26 -0700

    Interpolation and Differenctiation exercises

--- Week4/electrostatics.py
+++ Week4/electrostatics.py
@@ -1,11 +1,16 @@
 import numpy as np
 
-def pointPotential(x,y,q,Xc,Yc):
-	"""return the electrostatic potential"""
-	k = 8.9875518e9
-	v = (k*q)/(((x-Xc)**2.)+(y-Yc)**2.)**(1/2.) - ((k*q)/(((x-Xc)**2.)+(y+Yc)**2.)**(1/2.))
-	return v
+def pointPotential(x,y,q,posx,posy):
+    """returns the potential at x,y"""
+    k = 8.9875518*10**9
+    q = 1*10**-9
+    d=1
+    Vyx = (k*q)/((x-posx)**2 + (y-posy)**2)**(1./2.)
+    return Vyx
 
 def dipolePotential(x,y,q,d):
-	return pointPotential(x,y,q,d,0)
-
+    k = 8.9875518*10**9
+    q = 1*10**-9
+    d=1
+    Vxy = (k*q/((x)**2 + (y - d)**2)**(1/2.) - (k*q/(x**2 + (y + d)**2)**(1/2.))) #insert Vxy function
+    return Vxy
